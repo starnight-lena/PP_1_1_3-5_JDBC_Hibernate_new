2a22381b0f01c6373ca5cdd3286c5904288c5e26
Elena Kiseleva <elena.kiseleva91@yandex.ru>
2022-12-05 23:41:38 +0500

    commit test

--- src/main/java/jm/task/core/jdbc/Main.java
+++ src/main/java/jm/task/core/jdbc/Main.java
@@ -11,6 +11,7 @@ import java.sql.Connection;
 public class Main {
     public static void main(String[] args) {
         // реализуйте алгоритм здесь
+        //test
         Util util = new Util();
         util.getConnection();
         UserDao userDao = new UserDaoJDBCImpl();
